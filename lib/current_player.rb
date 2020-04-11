25d975533e42bc992a777079bc3bb8d6d473b7ed
Agnieszka Letkiewicz <a.letkiewicz@gmail.com>
2020-04-11 02:04:12 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,10 @@
 def turn_count(board)
   counter = 0 
   board.each do |space|
-    if #{space} == "X" 
+    if space == "X" || space ==  "O"
+      counter += 1 
+    else
+      counter += 0 
+    end
+    puts counter
 end
